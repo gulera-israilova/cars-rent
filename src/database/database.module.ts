2f7ec1d27a8cf6d69b1061e04fde5e32ecdb6065
Gulera Israilova <guleraisrailova@gmail.com>
2021-08-02 00:53:24 +0600

    Add env variables in database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,20 +4,22 @@ import {CarEntity} from "../cars/entity/car.entity";
 import {RentSessionEntity} from "../rent-sessions/entity/rent-session.entity";
 import {CarModule} from "../cars/car.module";
 import {RentSessionModule} from "../rent-sessions/rent-session.module";
+import { ConfigModule } from "@nestjs/config";
 
 
 @Module({
     imports: [
         CarModule,
         RentSessionModule,
+        ConfigModule.forRoot(),
         TypeOrmModule.forRoot(
         {
             "type": "postgres",
-            "host": "localhost",
-            "port": 5432,
-            "username": "postgres",
-            "password": "postgres",
-            "database": "postgres",
+            "host": process.env.DB_HOST,
+            "port": +process.env.DB_PORT,
+            "username": process.env.DB_USERNAME,
+            "password": process.env.DB_PASSWORD,
+            "database": process.env.DB_DATABASE,
             "autoLoadEntities": true,
             "synchronize": true,
             "entities": [CarEntity, RentSessionEntity]
